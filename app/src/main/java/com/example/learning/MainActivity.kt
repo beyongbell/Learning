fa3c158eeba51d36e7f6bf1ceec656940ee0ec72
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-04-07 16:38:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/learning/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.learning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnApply.setOnClickListener {
+            val firstName = etFirstName.text.toString()
+            val lastName = etLastName.text.toString()
+            val birthDate = etBirthDate.text.toString()
+            val country = etCountry.text.toString()
+            Log.d("MainActivity", "$firstName $lastName, born on $birthDate, from $country just applied to information.")
+        }
+    }
+}
